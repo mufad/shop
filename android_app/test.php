7bd087b9768c1ade0281e780a72fccb18bc0832f
Mihodi Hasan Lushan <mihodihasan@gmail.com>
2017-08-14 16:13:59 +0600

    Pain

--- android_app/test.php
+++ android_app/test.php
@@ -6,11 +6,23 @@
  * Time: 11:29 PM
  */
 require_once('knock_db_connect.php');
-$sql = "SELECT cart FROM oc_customer WHERE email='an registered user email';";
+$sql = "SELECT cart FROM oc_customer WHERE email='mihodihasan@gmail.com';";
 $result = mysqli_query($con, $sql);
 if (mysqli_num_rows($result) == 1) {
     $row = mysqli_fetch_assoc($result);
-    echo $row['cart'];
-    var_dump(unserialize(base64_decode($row['cart'])));
+
+    $cart_text= $row['cart'];
+    echo $cart_text;
 }
+$output_array=str_split($cart_text);
+for ($i=2;$i<sizeof($output_array);$i++){
+//    if ($output_array[$i]==)
+}
+$cartProductNumber= substr($cart_text,2,1);
+$pro1=substr($cart_text,11,44);
+$data=unserialize(base64_decode($pro1));
+echo $data['product_id'];
+
+//var_dump(unserialize(base64_decode("YToxOntzOjEwOiJwcm9kdWN0X2lkIjtpOjUwNDM7fQ==")));
+//var_dump(unserialize(base64_decode("YToxOntzOjEwOiJwcm9kdWN0X2lkIjtpOjUwNDc7fQ==")));
 
